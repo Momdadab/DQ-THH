b52601329ef3a59561c2de3189b7c303cc9eb547
THH2022 <165646718+THH2022@users.noreply.github.com>
2024-06-30 23:36:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1340,19 +1340,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ])
         
         reply_markup = InlineKeyboardMarkup(buttons)
-        current_time = datetime.now(pytz.timezone(TIMEZONE))
-        curr_time = current_time.hour        
-        if curr_time < 12:
-            gtxt = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 👋" 
-        elif curr_time < 17:
-            gtxt = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 👋" 
-        elif curr_time < 21:
-            gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
-        else:
-            gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        await client.edit_message_media(
-            query.message.chat.id, 
-            query.message.id, 
+        
             InputMediaPhoto(random.choice(PICS))
         )
         await query.message.edit_text(
@@ -1361,9 +1349,6 @@ async def cb_handler(client: Client, query: CallbackQuery):
             parse_mode=enums.ParseMode.HTML
         )
         await query.answer(MSG_ALRT)
-
-    elif query.data == "movie_suggestion":
-        await query.answer(url=f"https://t.me/{temp.U_NAME}?start=movie_suggestion")
         
     elif query.data == "show_reff":
         user_id = query.from_user.id
